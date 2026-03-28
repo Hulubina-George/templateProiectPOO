8b5654b3415ff22d0cde90b276b558b6438b8881
Hulubina George <ghulubina@gmail.com>
2026-03-28 20:14:06 +0200

    Modificare

--- main.cpp
+++ main.cpp
@@ -1,16 +1,10 @@
 #include "Sala.h"
 #include "Meniu.h"
-#include <iostream>
 
 int main() {
-    try {
-        Sala salaMea("Gym");
-        Meniu meniu(salaMea);
-        meniu.ruleaza();
-    }
-    catch (const std::exception& e) {
-        std::cerr << "Eroare fatala: " << e.what() << std::endl;
-        return 1;
-    }
+    Sala salaMea("Gym");
+    Meniu meniu(salaMea);
+    meniu.ruleaza();
+
     return 0;
 }
