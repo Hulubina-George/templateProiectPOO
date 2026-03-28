97e158bc556cf51f2e832480d27223698497cec9
Hulubina George <ghulubina@gmail.com>
2026-03-28 20:32:43 +0200

    Final

--- Meniu.cpp
+++ Meniu.cpp
@@ -18,90 +18,91 @@ void Meniu::ruleaza() {
 
         switch (optiune) {
             case 1: {
-                int sub;
+                int suboptiune;
                 std::cout << "\n1.Client 2.Fitness 3.Nutritie 4.Dublu 5.Echipament\nAlegere: ";
-                if (!(std::cin >> sub)) break;
+                if (!(std::cin >> suboptiune)) break;
                 std::cin.ignore();
 
                 try {
-                    if (sub >= 1 && sub <= 4) {
+                    if (suboptiune >= 1 && suboptiune <= 4) {
                         std::string nume;
                         std::cout << "Nume: ";
                         if (!std::getline(std::cin, nume)) break;
-                        int v;
+                        int varsta;
                         std::cout << "Varsta: ";
-                        if (!(std::cin >> v)) break;
+                        if (!(std::cin >> varsta)) 
+                            break;
 
-                        if (sub == 1) {
-                            int viz;
+                        if (suboptiune == 1) { //client
+                            int vizite;
                             std::cout << "Vizite: ";
-                            if (!(std::cin >> viz)) break;
-                            salaMea.adaugapersoana(new Client(v, nume, viz, Abonament("Standard", 150, 30)));
-                        } else if (sub == 2) {
-                            double s;
+                            if (!(std::cin >> vizite)) break;
+                            salaMea.adaugapersoana(new Client(varsta, nume, vizite, Abonament("Standard", 150, 30)));
+                        } else if (suboptiune == 2) { //fitness
+                            double salariu;
                             std::cout << "Salariu: ";
-                            if (!(std::cin >> s)) break;
+                            if (!(std::cin >> salariu)) break;
 
-                            if (s < 0 || s > 10000) throw ExceptieValidare("Salariu invalid! Trebuie să fie între 0 și 10000.");
+                            if (salariu < 0 || salariu > 10000) throw ExceptieValidare("Salariu invalid! Trebuie să fie între 0 și 10000.");
                             
                             std::cout << "Antrenament: ";
                             std::cin.ignore();
-                            std::string ant;
-                            if (!std::getline(std::cin, ant)) break;
-                            salaMea.adaugapersoana(new AngajatFitness(v, nume, s, ant));
-                        } else if (sub == 3) {
-                            double s;
+                            std::string antrenament;
+                            if (!std::getline(std::cin, antrenament)) break;
+                            salaMea.adaugapersoana(new AngajatFitness(varsta, nume, salariu, antrenament));
+                        } else if (suboptiune == 3) { //nutritie
+                            double salariu;
                             std::cout << "Salariu: ";
-                            if (!(std::cin >> s)) break;
+                            if (!(std::cin >> salariu)) break;
 
-                            if (s < 0 || s > 10000) throw ExceptieValidare("Salariu invalid! Trebuie să fie între 0 și 10000.");
+                            if (salariu < 0 || salariu > 10000) throw ExceptieValidare("Salariu invalid! Trebuie să fie între 0 și 10000.");
 
                             std::cout << "Dieta: ";
                             std::cin.ignore();
                             std::string dieta;
                             if (!std::getline(std::cin, dieta)) break;
-                            salaMea.adaugapersoana(new AngajatNutritie(v, nume, s, dieta));
-                        } else if (sub == 4) {
-                            double s;
+                            salaMea.adaugapersoana(new AngajatNutritie(varsta, nume, salariu, dieta));
+                        } else if (suboptiune == 4) { //dublu
+                            double salariu;
                             std::cout << "Salariu: ";
-                            if (!(std::cin >> s)) break;
+                            if (!(std::cin >> salariu)) break;
 
-                            if (s < 0 || s > 10000) throw ExceptieValidare("Salariu invalid! Trebuie să fie între 0 și 10000.");
+                            if (salariu < 0 || salariu > 10000) throw ExceptieValidare("Salariu invalid! Trebuie să fie între 0 și 10000.");
 
                             std::cin.ignore();
-                            std::string ant, dieta;
+                            std::string antrenament, dieta;
                             std::cout << "Antrenament: ";
-                            if (!std::getline(std::cin, ant)) break;
+                            if (!std::getline(std::cin, antrenament)) break;
                             std::cout << "Dieta: ";
                             if (!std::getline(std::cin, dieta)) break;
-                            salaMea.adaugapersoana(new AngajatDublu(v, nume, s, ant, dieta));
+                            salaMea.adaugapersoana(new AngajatDublu(varsta, nume, salariu, antrenament, dieta));
                         }
                         std::cout << "Succes!\n";
-                    } else if (sub == 5) {
-                        std::string nE;
+                    } else if (suboptiune == 5) { //echipament
+                        std::string numeEchipament;
                         std::cout << "Nume echipament: ";
-                        if (std::getline(std::cin, nE)) {
-                            salaMea.adaugaechipament(Echipament(nE));
+                        if (std::getline(std::cin, numeEchipament)) {
+                            salaMea.adaugaechipament(Echipament(numeEchipament));
                         }
                     }
                 } catch (const std::exception& e) { std::cout << "Eroare: " << e.what() << "\n"; }
                 break;
             }
             case 2: {
-                std::string nC;
-                double sN;
+                std::string numeAngajat;
+                double salariuNou;
                 std::cout << "Nume angajat: ";
                 std::cin.ignore();
-                if (!std::getline(std::cin, nC)) break;
+                if (!std::getline(std::cin, numeAngajat)) break;
                 std::cout << "Salariu nou: ";
-                if (!(std::cin >> sN)) break;
+                if (!(std::cin >> salariuNou)) break;
 
                 try {
-                    if (sN < 0 || sN > 10000) {
+                    if (salariuNou < 0 || salariuNou > 10000) {
                         throw ExceptieValidare("Salariu invalid! Trebuie sa fie intre 0 si 10000.");
                     }
-                    
-                    if (salaMea.modificaSalariu(nC, sN)) {
+
+                    if (salaMea.modificaSalariu(numeAngajat, salariuNou)) {
                         std::cout << "Actualizat!\n";
                     } else {
                         std::cout << "Angajatul nu a fost găsit!\n";
@@ -116,10 +117,10 @@ void Meniu::ruleaza() {
                 break;
             case 4:
                 salaMea.stergeUltimaPersoana();
-                std::cout << "Sters!\n";
+                std::cout<<"Sters!"<<'\n';
                 break;
             case 0:
-                std::cout << "Iesire...\n";
+                std::cout<<"Iesire"<<'\n';
                 break;
         }
     }
